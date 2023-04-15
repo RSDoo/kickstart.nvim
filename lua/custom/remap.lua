2f5d2532d7cf4bc81b771ab6e2352e63c5206072
Robin Dünnwald <robin.duennwald@gmail.com>
2023-04-15 23:14:02 +0200

    remap copilot

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -1,3 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.g.copilot_assume_mapped = true
+
+-- copilot 
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-H>", 'copilot#Previous()', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-K>", 'copilot#Next()', { silent = true, expr = true })
