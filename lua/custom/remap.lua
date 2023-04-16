b0f9b6f43a03cc82a5c4c6e8e9e5499871afe2a1
Robin Dünnwald <robin.duennwald@gmail.com>
2023-04-16 16:06:58 +0200

    add code formatter

--- lua/custom/remap.lua
+++ lua/custom/remap.lua
@@ -2,7 +2,12 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.g.copilot_assume_mapped = true
 
--- copilot 
+-- copilot
 vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 vim.api.nvim_set_keymap("i", "<C-H>", 'copilot#Previous()', { silent = true, expr = true })
 vim.api.nvim_set_keymap("i", "<C-K>", 'copilot#Next()', { silent = true, expr = true })
+
+
+-- format with mhartington/formatter.nvim
+vim.keymap.set("n", "<leader>ff", ":Format<CR>")
+vim.keymap.set("n", "<leader>F", ":FormatWrite<CR>")
